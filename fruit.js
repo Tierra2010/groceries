3cfe0634fbbc7c0e5a1ca27f66b9d010a94381f2
Tierra2010 <105245676+Tierra2010@users.noreply.github.com>
2022-07-16 22:18:33 -0500

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,81 @@
+<!-- List of Text Items -->
+<
+ion - list >
+    <
+    ion - item >
+    <
+    ion - label > Oranges < /ion-label> <
+    /ion-item> <
+    ion - item >
+    <
+    ion - label > Apples X < /ion-label> <
+    /ion-item> <
+    ion - item >
+    <
+    ion - label > Strawberries < /ion-label> <
+    /ion-item> <
+    ion - item >
+    <
+    ion - label > Mango < /ion-label> <
+    /ion-item> <
+    ion - item >
+    <
+    ion - label > Grapes < /ion-label> <
+    /ion-item> <
+    /ion-list>
+
+<!-- List of Input Items -->
+<
+ion - list >
+    <
+    ion - item >
+    <
+    ion - label > Input < /ion-label> <
+    ion - input > < /ion-input> <
+    /ion-item> <
+    ion - item >
+    <
+    ion - label > Toggle < /ion-label> <
+    ion - toggle slot = "end" > < /ion-toggle> <
+    /ion-item> <
+    ion - item >
+    <
+    ion - label > Radio < /ion-label> <
+    ion - radio slot = "end" > < /ion-radio> <
+    /ion-item> <
+    ion - item >
+    <
+    ion - label > Checkbox < /ion-label> <
+    ion - checkbox slot = "start" > < /ion-checkbox> <
+    /ion-item> <
+    /ion-list>
+
+<!-- List of Sliding Items -->
+<
+ion - list >
+    <
+    ion - item - sliding >
+    <
+    ion - item >
+    <
+    ion - label > Item < /ion-label> <
+    /ion-item> <
+    ion - item - options side = "end" >
+    <
+    ion - item - option onClick = "unread(item)" > Unread < /ion-item-option> <
+    /ion-item-options> <
+    /ion-item-sliding>
+
+<
+ion - item - sliding >
+    <
+    ion - item >
+    <
+    ion - label > Item < /ion-label> <
+    /ion-item> <
+    ion - item - options side = "end" >
+    <
+    ion - item - option onClick = "unread(item)" > Unread < /ion-item-option> <
+    /ion-item-options> <
+    /ion-item-sliding> <
+    /ion-list>
